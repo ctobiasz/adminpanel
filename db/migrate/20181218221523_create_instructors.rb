c38b07730010acde92946e1d4f46f88b70f1a167
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2018-12-18 17:59:51 -0500

    made model tables

---
+++ db/migrate/20181218221523_create_instructors.rb
@@ -0,0 +1,14 @@
+class CreateInstructors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :instructors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.float :salary
+      t.string :education
+      t.integer :cohort_id
+
+      t.timestamps
+    end
+  end
+end
